1367d73649ff8f130b7f12878d660152fdedecdf
Harshavardhana Shastri <harshavardhana.shastri@velocomputing.tech>
2026-04-08 19:23:57 +0200

    Add comments to kernel entry assembly code

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,5 +1,5 @@
 [bits 32]
 [extern kernel_main]
 
-call kernel_main
-jmp $
+call kernel_main    ; Jump into our C++ main.cpp execution!
+jmp $               ; Infinite loop if the kernel ever unexpectedly exits
