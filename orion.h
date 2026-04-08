1af3fe89c99291030f7163b4c2279f50b336bd64
Harshavardhana Shastri <harshavardhana.shastri@velocomputing.tech>
2026-04-08 19:16:23 +0200

    Refactor comments and update graphics buffer declaration

--- orion.h
+++ orion.h
@@ -1,45 +1,45 @@
 #ifndef ORION_H
 #define ORION_H
 
-// Bare-metal standard types (since we don't have <stdint.h>)
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned int size_t;
 
-// Dummy snprintf signature to satisfy the mbedTLS mockup in login.cpp
+// Dummy snprintf for the login mockup
 extern "C" int snprintf(char * s, size_t n, const char * format, ...);
 
-// --- Graphics Core (Implemented in system.cpp) ---
-extern uint8_t* vga_graphics_buffer;
+// Core Graphics (in system.cpp)
+extern uint8_t double_buffer[320 * 200];
 void draw_pixel(int x, int y, uint8_t color);
 void draw_rect(int x, int y, int width, int height, uint8_t color);
 void draw_logo(int start_x, int start_y);
+void flush_buffer_to_screen();
 
-// --- KAN-1: Supabase Login (login.cpp) ---
+// KAN-1: Login
 void render_login_screen();
 bool is_authenticated();
 
-// --- KAN-4: Home Screen (main.cpp) ---
+// KAN-4: Home
 void render_home_screen();
 
-// --- KAN-5: App View (app.cpp) ---
+// KAN-5: App View
 void render_app_view();
 
-// --- KAN-6: Terminal (terminal.cpp) ---
+// KAN-6: Terminal
 void render_terminal();
 
-// --- KAN-7: Default Settings (default.cpp) ---
+// KAN-7: Defaults
 extern uint8_t theme_bg_color;
 extern uint8_t theme_taskbar_color;
 extern uint8_t theme_text_color;
 void load_defaults();
 
-// --- KAN-8: System Apps (system.cpp) ---
+// KAN-8: System
 void render_taskbar();
 void init_system();
 
-// --- Settings & Info (settings.cpp) ---
+// Settings
 void render_settings();
 
 #endif
